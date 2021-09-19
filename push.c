f4a747a725644d0e5fe45e72b92c4ea5dbf21373
evgeniya-burlachenko <e.accounted@gmail.com>
2021-09-19 15:19:57 +0300

    pa pb

---
+++ push.c
@@ -0,0 +1,38 @@
+#include "push_swap.h"
+
+void	pa(t_arr *a_arr, t_arr *b_arr, t_info *data)
+{
+	int i;
+
+	i = data->a_len;
+	while(i-- > 0)
+		a_arr[i + 1] = a_arr[i];
+	a_arr[0] = b_arr[0];
+	i = 0;
+	while (i++ <= data->b_len - 1)
+		b_arr[i - 1] = b_arr[i];
+	data->a_len++;
+	data->b_len--;
+	data->count++;
+	if (data->print == 2)
+		ft_putstr_fd("pa\n", 1);
+}
+
+void pb (t_arr *a_arr, t_arr *b_arr, t_info *data)
+{
+	int	i;
+
+	i = data->b_len;
+	while (i-- > 0)
+		b_arr[i + 1] = b_arr[i];
+	b_arr[0] = a_arr[0];
+	i = 0;
+	while(i++ <= data->a_len - 1)
+		a_arr[i - 1] = a_arr[i];
+	data->a_len--;
+	data->b_len++;
+	data->count++;
+	if(data->print == 2)
+		ft_putstr_fd("pb\n", 1);
+	
+}
